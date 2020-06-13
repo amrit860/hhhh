a16a509f874d1ae39a2123782d57a3adf202d43d
amrit860 <you@example.com>
2020-06-13 12:11:55 +0545

    jestha31

--- modules/products/product.controller.js
+++ modules/products/product.controller.js
@@ -1,10 +1,10 @@
 const ProductQuery = require('./product.query');
 
 function get(req, res, next) {
-//     const conditin ={};
-// if(req.loggedInUser.rle != 1){
-//     cndition.vender=req.loggedInUser._id;
-// }
+    //     const conditin ={};
+    // if(req.loggedInUser.role != 1){
+    //     condition.vender=req.loggedInUser._id;
+    // }
     ProductQuery
         .find({})
         .then(function (data) {
@@ -13,14 +13,14 @@ function get(req, res, next) {
         .catch(function (err) {
             next(err);
         })
-        console.log("product get>>",res);
+    console.log("product get>>", res);
 }
 
 function post(req, res, next) {
     console.log('req.body', req.body);
-    console.log("req.file>>",req.file);
+    console.log("req.file>>", req.file);
 
-    console.log("req.files>>",req.files);
+    console.log("req.files>>", req.files);
     const data = req.body;
     // data.vender=req.loggedInUser._id;
     // if(req.fileErr){
@@ -45,7 +45,7 @@ function getById(req, res, next) {
     ProductQuery
         .find(condition)
         .then(function (data) {
-            res.status(200).json(data[0]);
+            res.status(200).json(data);
         })
         .catch(function (err) {
             next(err);
@@ -58,65 +58,89 @@ function search(req, res, next) {
     const data = req.body;
     const searchCondition = ProductQuery.map_product_req(condition, data);
     console.log('search >>', searchCondition);
-    ProductQuery
-        .find(condition)
-        .then(function (data) {
-            res.status(200).json(data);
-        })
-        .catch(function (err) {
-            next(err);
-        })
-}
+    if (req.body.minPrice) {
+        searchCondition.price = {
+            $gte: req.body.minPrice
+        }
 
-
-
-
-function put(req, res, next) {
-    const data = req.body;
-    data.vender=req.loggedInUser._id;
-    if(req.fileErr){
-        return next({
-            msg:"invalid file format"
-        })
-    }
-    ProductQuery
-        .update(req.params.id, data)
-        .then(function (data) {
-            if (req.file) {
-                // remove existing file
-                fs.unlink(path.join(process.cwd(), 'uploads/images/' + data.oldImages[0]), function (err, done) {
-                    if (err) {
-                        console.log('err deleting file');
-                    }
-                    else {
-                        console.log('file removed');
-                    }
-                });
-                console.log('data.oldImages', data.oldImages);
-
+        if (req.body.maxPrice) {
+            searchCondition.price = {
+                $lte: req.body.maxPrice
             }
-            res.status(200).json(data);
-        })
-        .catch(function (err) {
-            next(err);
-        })
-}
-function remove(req, res, next) {
-    ProductQuery
-        .remove(req.params.id)
-        .then(function (data) {
-            res.status(200).json(data);
-        })
-        .catch(function (err) {
-            next(err);
-        })
-}
+        }
+        if (req.body.minPrice && req.body.maxPrice) {
+            searchCondition.price = {
+                $gte: req.body.minPrice,
+                $lte: req.body.maxPrice
+            }
+        }
+        if(req.body.fromDate && req.body.toDate){
+                const fromDate=new Date(req.body.fromDate).setHours(0,0,0,0);
+                const toDate=new Date(req.body.toDate).setHours(23,59,59,999);
+                searchCondition.ceatedAt={
+                    $lte:new Date(toDate),
+                    $gte:new Date(fromDate)
+                }
+        }
+        ProductQuery
+            .find(condition)
+            .then(function (data) {
+                res.status(200).json(data);
+            })
+            .catch(function (err) {
+                next(err);
+            })
+    }
 
-module.exports = {
-    get,
-    getById,
-    search,
-    post,
-    put,
-    remove
-}
+
+
+
+    function put(req, res, next) {
+        const data = req.body;
+        data.vender = req.loggedInUser._id;
+        if (req.fileErr) {
+            return next({
+                msg: "invalid file format"
+            })
+        }
+        ProductQuery
+            .update(req.params.id, data)
+            .then(function (data) {
+                if (req.file) {
+                    // remove existing file
+                    fs.unlink(path.join(process.cwd(), 'uploads/images/' + data.oldImages[0]), function (err, done) {
+                        if (err) {
+                            console.log('err deleting file');
+                        }
+                        else {
+                            console.log('file removed');
+                        }
+                    });
+                    console.log('data.oldImages', data.oldImages);
+
+                }
+                res.status(200).json(data);
+            })
+            .catch(function (err) {
+                next(err);
+            })
+    }
+    function remove(req, res, next) {
+        ProductQuery
+            .remove(req.params.id)
+            .then(function (data) {
+                res.status(200).json(data);
+            })
+            .catch(function (err) {
+                next(err);
+            })
+    }
+
+    module.exports = {
+        get,
+        getById,
+        search,
+        post,
+        put,
+        remove
+    }
